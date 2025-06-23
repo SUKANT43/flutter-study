a327ca2cd9d38dde6880b9acc3ef42ed3ed05f75
sukant43 <sukant98657@gmail.com>
2025-06-23 23:36:52 +0530

    i am rich added

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(
-          child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/i_am_rich_app_icon.png')),
         ),
       ),
     ),
